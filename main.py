a236cc67004fcaf94dce87ca3dfecdbf2b0819a5
Sairam <mstrthedespicable@gmail.com>
2023-02-01 11:21:42 +0530

    added descrption in a new line

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ with col1:
 with col2:
     st.title("Saithulasiram M")
     content = """Hi, I am Saithulasiram Welcome to my website. I am working as a System Admin in Kyndryl I have been
-    practising Python from Udemy for quite a while now. I like coding but the most fun part is when an issue comes
-    with my code, I like investigating and solving the error on my own.
+    practising Python from Udemy for quite a while now. I like coding but the :red[most fun] part is when an issue comes
+    with my code, I like investigating and solving the error on my own :sunglasses:.
          
     """
     st.info(content)
 
+
 st.text("""
 This page contains python project that I have completed and that I am working on. Go on and take a look!!!
 """)
-st.balloons()
+
