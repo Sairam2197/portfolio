44a9800f10501f519602900b33107042dfaf3aac
Sairam <mstrthedespicable@gmail.com>
2023-02-01 11:16:37 +0530

    added descrption in a new line

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import streamlit as st
+from datetime import datetime
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
+    st.image("images/photo.jpg", caption='Thinking whether to use for loop or while loop in my next program', width=500)
 
 with col2:
     st.title("Saithulasiram M")
-    content = """Hi, I am Saithulasiram Welcome to my website. This page contains python project that I have completed
-    and that I am working on. Go on and take a look!!!    
+    content = """Hi, I am Saithulasiram Welcome to my website. I am working as a System Admin in Kyndryl I have been
+    practising Python from Udemy for quite a while now. I like coding but the most fun part is when an issue comes
+    with my code, I like investigating and solving the error on my own.
+         
     """
     st.info(content)
+
+st.text("""
+This page contains python project that I have completed and that I am working on. Go on and take a look!!!
+""")
+st.balloons()
