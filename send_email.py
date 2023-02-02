f0b99a8eb366883e38f24f15167ef8b3ea6e270b
Sairam <mstrthedespicable@gmail.com>
2023-02-02 17:02:29 +0530

    added requirements.txt

--- send_email.py
+++ send_email.py
@@ -1,13 +1,14 @@
 import smtplib
 import ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "portfoliosite98@gmail.com"
-    password = "kkzssanqjgzwxchv"
+    password = os.getenv("PASSWORD")
+
 
     receiver = "portfoliosite98@gmail.com"
     context = ssl.create_default_context()
